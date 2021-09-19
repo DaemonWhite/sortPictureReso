a51c2e08fb14d202aa313a6102cc998872a8c19a
DaemonWhite <44682988+DaemonWhite@users.noreply.github.com>
2021-09-19 23:54:00 +0200

    Debut de inteligent path

--- trie.sh
+++ trie.sh
@@ -1,10 +1,10 @@
-path="/home/matheo/Images/Font d'écran";
-inPath="$path/a trier"
-
-pcPath="$path/tmp pc"
-mixPath="$path/tmp mixt"
-mobPath="$path/tmp mob"
-autPath="$path/tmp autre"
+# Chemin par default pour chercher les images
+inPath="$HOME/Images/Font d'écran/a trier"
+# Chemin par default ou seron ranger les immage
+pcPath="$HOME/Images/Font d'écran/tmp pc"
+mixPath="$HOME/Images/Font d'écran/tmp mixt"
+mobPath="$HOME/Images/Font d'écran/tmp mob"
+autPath="$HOME/Images/Font d'écran/tmp autre"
 
 #Initialise les donner
 initialise=0;
@@ -17,6 +17,127 @@ iListeW=$initialise;
 pc=1.9
 mixte=1.5
 mobile=0.9
+
+if [[ -f "$HOME/.trieCache" ]]; then
+
+  source "$HOME/.trieCache"
+  echo "inPath=\"$inPath\"" >> $HOME/.trieCache
+
+fi
+
+
+
+installVerif=false;# Si vraie il ne créra pas les dossier si il n'existe pas
+
+help()
+{
+  echo "
+  -i  --install   configure des nouveau chemin fixe
+  -u  --uninstall suprime les chemin fixe installer
+  -s  --secure    copie les fichier au lieux de les deplacer
+  -p  --path      defini le chemin d'accer
+  -o  --output    definie le chemin de sortie
+  -f  --format    Definie les valeur des format
+    {pc} {mixte} {mobile}
+  -h  --help      information commande
+  "
+}
+
+install()
+{
+
+  while [[ 1 ]]; do
+
+  echo "Voulez vous qu'on crées les chemins si ils n'existent? [y/n]"
+
+  read repsInsVerif
+
+    case $repsInsVerif in
+         # Parameters that don't require value
+       n | n )
+         installVerif=false; break ;;
+       y | Y | o | O)
+         installVerif=true; break ;;
+       *)
+         echo "Valeur incorecte"; shift ;;
+    esac
+
+  done
+
+  if [[ -f "$HOME/.trieCache" ]]; then
+
+    rm "$HOME/.trieCache"
+      
+  fi
+
+  touch "$HOME/.trieCache"
+
+  echo "Donner le chemin ou son vos immage à trouver"
+
+  read inPath
+
+  echo "Donner le chemin ou vous voulez envoyer vos Image foramt pc"
+
+  echo "Donner le chemin ou vous voulez envoyer vos Image foramt mixte"
+
+  echo "Donner le chemin ou vous voulez envoyer vos Image foramt mobile"
+
+  echo "";
+
+  while [[ 1 ]]; do
+
+     echo "Voulez vous changer les valeur par defaut pour le trie des image [y/n] -i pour plus d'info
+pc=1.9
+mixte=1.5
+mobile=0.9
+"  
+    read repsInsValue
+
+    case $repsInsValue in
+         # Parameters that don't require value
+       -i | -I | --info )
+         echo "
+Les valeurs son caculer a partir de leur fraction exemple 16/9 fait 1.78 plus la valeur sera petite plus le format serat en portrait(etroit) et inversemnt plus il sera grand plus le foramt sera proche d'un paysage(Large)
+
+Les donner sont rangée dans cette ordre 'Aurtre > PC > Mixte > mobile' tout ce qui sera supérieur à 1.9 sera donc mit dans 'autre' si on prend les valeur par défaut les immage pc devron êtres inferieur à 1.9 et supéreiurs à 1.5
+"; shift ;;
+       n | n )
+         repsInsValue=true; break ;;
+       y | Y | o | O)
+         repsInsValue=true; break ;;
+       *)
+         echo "Valeur incorecte"; shift ;;
+    esac
+
+
+
+  done;
+
+
+}
+
+#Demande l'avis à l'utilisateur
+
+while [[ $# -gt 0 ]]; do
+
+
+  case "${1}" in
+      # Parameters that don't require value
+    -i|--install)
+      install; shift ;;
+    -p|--path)
+      echo "coucou ${2}"; shift ;;
+    -h|--help)
+      help; shift ;;
+    *)
+      has_any_error="true"; shift ;;
+  esac
+
+  #echo "$# et ${1}"
+
+done
+
+exit
 
 #parité
 br=0
