cfbc1678b7ef31c8608579eacda6cd0825ad47be
DaemonWhite <matheo.daemon@gmail.com>
2023-07-31 17:19:23 +0200

    Virtual environement

--- PictureSorter.py
+++ PictureSorter.py
@@ -1,56 +1,19 @@
 #!/bin/python3
 
-class Size_image_storage(object):
+from SizeImageStorage import Size_image_storage
 
-    def __init__(self):
-        self.__coefficient = dict()
-
-    def get_coef(self, name_coef: str):
-        return self.__coefficient[name_coef]
-
-    def get_dict_coeff(self):
-        return self.__coefficient.copie()
-
-    def default_coef(self):
-        self.__coefficient = {
-            "pc-statdart" : [1.5, 1.9],
-            "pc-old" : [0.9, 1.5],
-            "mobile" : [0.0, 0.9],
-        }
-
-    def add_coef(self, name_coef: str, min_coef: float, max_coef: float):
-        self.__coefficient[name_coef] = [min_coef, max_coef]
-
-    def add_auto_coef_by_resolution(
-        self,
-        name_coef: str,
-        min_width: int,
-        min_height: int,
-        max_width: int,
-        max_height: int):
-
-        low_coef = self.calculate_coef(min_width, min_height)
-        max_coef = self.calculate_coef(max_width, max_height)
-
-        self.add_coef(name_coef, low_coef, max_coef)
-
-    def remove_coef(self, name_coef):
-        del self.__coefficient[name_coef]
-
-    def list_coef(self):
-        print(self.__coefficient)
-
-    def calculate_coef(width: int, height: int) -> float:
-        coef = width / height
-        return coef
+from xdg_base_dirs import (
+    xdg_cache_home,
+    xdg_config_dirs,
+    xdg_config_home,
+    xdg_data_dirs,
+    xdg_data_home,
+    xdg_runtime_dir,
+    xdg_state_home,
+)
 
 if __name__ == "__main__":
-    te = Size_image_storage()
-    te.default_coef()
-    te.add_coef("pc-large", 1.9, 2.3)
-    te.remove_coef("pc-statdart")
-    te.list_coef()
-    print( "Pc Large : ", te.get_coef("pc-large") )
+    print(xdg_state_home())
 
 class Picture_sorter(Size_image_storage):
 
--- dev_env.sh
+++ dev_env.sh
@@ -1,6 +1,14 @@
-#!/bin/python3
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+VIRTUAL_ENV="virtual_PictureSorter"
 
 function init()
 {
-
+	python3 -m venv $VIRTUAL_ENV
+	source $VIRTUAL_ENV/bin/activate
+	pip install -r requirements.txt
 }
+
+init
