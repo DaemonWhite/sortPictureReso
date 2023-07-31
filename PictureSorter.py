6c473df41f1c02c9ab2bb395379dcf1b2fb5a9cd
DaemonWhite <matheo.daemon@gmail.com>
2023-07-31 20:54:51 +0200

    add use xdg linux picture

--- PictureSorter.py
+++ PictureSorter.py
@@ -10,41 +10,77 @@ import xdg.BaseDirectory
 
 class Picture_sorter(Size_image_storage):
 
-    def __init__(self):
-        self.__picture_path = self.default_path_picture()
-        self.__load_picture_path = str
-        self.__path_images_save = str
-        self.__path_images_load = str
+    DEFAULT_PICTURE_OUT = "./"
 
+    def __init__(self,
+        picture_in = "",
+        picutre_out = ""):
 
-    def default_path_picture(self) -> str:
-        path_picture = "./" # Si aucun système n'est detecter
+        self.__picture_in = self.DEFAULT_PICTURE_OUT
+        self.__picture_out = self.DEFAULT_PICTURE_OUT
+
+        self.set_picture_in_path(picture_in)
+        self.set_picture_out_path(picutre_out)
+
+    def get_picture_in_path(self):
+        return self.__picture_in
+
+    def get_picture_out_path(self):
+        return self.__picture_out
+
+    def set_picture_in_path(self, path: str):
+        if path == "":
+            self.default_in_picture()
+
+    def set_picture_out_path(self, path: str):
+        if path == "":
+            self.default_out_picture()
+
+    def default_in_picture(self):
+        path_picture = self.xdg_picture_path()
+
+        self.__picture_in = path_picture
+
+    def default_out_picture(self):
+        path_picture = self.xdg_picture_path() + "/out" # Si aucun système n'est detecter
+
+        self.__picture_out = path_picture
+
+    def xdg_picture_path(self) -> str:
+        path_picture = self.DEFAULT_PICTURE_OUT
         system = platform.system()
 
         if system == "Windows":
-            path_picture = self.get_picture_path_windows()
+            path_picture = self.xdg_picture_path_windows()
         elif system == "Linux":
-            path_picture = self.get_picture_path_linux()
+            path_picture = self.xdg_picture_path_linux()
 
         return path_picture
 
-    def get_picture_path_linux(self):
+    def xdg_picture_path_linux(self):
+        DIRECTORY_NAME = "PICTURES"
+        xdg_config_home = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
+        xdg_user_dirs_file = os.path.join(xdg_config_home, 'user-dirs.dirs')
+
+        if not os.path.exists(xdg_user_dirs_file):
+            raise FileNotFoundError("XDG user directories configuration file not found.")
+
+        with open(xdg_user_dirs_file, 'r') as f:
+            for line in f:
+                if line.startswith(f'XDG_{DIRECTORY_NAME}'):
+                    path = os.path.expanduser(line.split('=', 1)[1].strip().strip('"'))
+                    path_find = path.find('$HOME/')
+                    if path_find > -1:
+                        return line.split('$HOME/', 1)[1].strip().strip('"')
+                    else:
+                        return path.strip('"')
+
+
+
+
+    def xdg_picture_path_windows(self):
         pass
 
-    def get_picture_path_windows(self):
-        pass
-
-    def get_picture_path(self):
-        return self.__picture_path
-
-    def get_load_picture_path(self):
-        return self.__load_picture_path
-
-    def set_picture_path(self, path: str):
-        self.__picture_path = path
-
-    def set_load_picture_path(self, path: str):
-        self.__load_picture_path = default_path_picture
 
     def resolve(self):
         pass
@@ -54,28 +90,8 @@ class Picture_sorter(Size_image_storage):
 
 if __name__ == "__main__":
     ps = Picture_sorter()
-    #print (xdg_user_dirs())
+    print("Path in : ", ps.get_picture_in_path())
+    print("Path in : ", ps.get_picture_out_path())
 
 import os
-
-# def get_xdg_user_dir(directory_name):
-#     xdg_config_home = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
-#     xdg_user_dirs_file = os.path.join(xdg_config_home, 'user-dirs.dirs')
-
-#     if not os.path.exists(xdg_user_dirs_file):
-#         raise FileNotFoundError("XDG user directories configuration file not found.")
-
-#     with open(xdg_user_dirs_file, 'r') as f:
-#         for line in f:
-#             if line.startswith(f'XDG_{directory_name.upper()}'):
-#                 return os.path.expanduser(line.split('=', 1)[1].strip().strip('"'))
-
-#     raise ValueError(f"XDG user directory '{directory_name}' not found in the configuration file.")
-
-# Example usage
-# desktop_dir = get_xdg_user_dir('DESKTOP')
-# documents_dir = get_xdg_user_dir('DOCUMENTS')
-
-# print(f"Desktop directory: {desktop_dir}")
-# print(f"Documents directory: {documents_dir}")
 
