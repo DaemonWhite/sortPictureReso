58f52e98a178b0a56984ad6d4b3e6d9fab19d988
DaemonWhite <matheo.daemon@gmail.com>
2023-08-01 16:14:03 +0200

    Fix picture lib

--- src/LibPictureSorter/__init__.py
+++ src/LibPictureSorter/__init__.py
@@ -1,4 +1,4 @@
-from .sizeimagestorage import Size_image_storage
 from .picturesorter import Picture_sorter
+from .sizeimagestorage import Size_image_storage
 
-__all__ = ["Size_image_storage", "Picture_sorter"]
+__all__ = ["Picture_sorter", "Size_image_storage"]
