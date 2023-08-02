3a5858ed23f68924df7c59ae968eb98574ea0ba0
DaemonWhite <matheo.daemon@gmail.com>
2023-08-02 17:33:37 +0200

    add setup.py

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pictureSorter',
+    version='0.0.3',
+    description='Mon super module Python',
+    author='DaemonWhite',
+    author_email='votre@email.com',
+    packages=find_packages(),
+)
